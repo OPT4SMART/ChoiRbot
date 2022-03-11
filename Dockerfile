d3e5fd165f75ae5fda9d7ffa56128dfaa7ff2d9f
acharya <srikrishna3118@gmail.com>
2022-03-11 06:30:27 +0530

    added docker-compose.yml Dockerfile bringup_dashing_devel.sh to setup development container.

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+FROM ros:dashing
+
+USER root
+
+RUN . /opt/ros/dashing/setup.sh && \
+    apt update -qq && apt install -y -q \
+    build-essential \
+    cmake \
+    git \
+    apt-transport-https \
+    python3-colcon-common-extensions \
+    software-properties-common \
+    openssh-client \
+    gnupg2 \
+    python3-pip \
+    wget \
+    && rm -rf /var/lib/apt/lists/* \
+
+RUN locale-gen en_US.UTF-8; dpkg-reconfigure -f noninteractive locales
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US.UTF-8
+ENV LC_ALL en_US.UTF-8
+
+RUN pip3 install -U pip
+RUN pip3 install -U setuptools
+
+RUN sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list' && \
+    wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add - && \
+    apt-get update && \
+    sudo apt-get install -y gazebo11 &&\
+    rm -rf /var/lib/apt/lists/* \
+
+
+# Clone all the necessary sources and install deps through rosdep.
+RUN wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc -O - | sudo apt-key add - && \
+    sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+RUN groupadd -g 1000 rbccps && \
+    useradd -d /home/rbccps -s /bin/bash -m rbccps -u 1000 -g 1000 && \
+    usermod -aG sudo rbccps && \
+    echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+USER rbccps
+RUN mkdir -p /home/rbccps/mrs_ws/src
+ENV HOME /home/rbccps
+WORKDIR /home/rbccps/mrs_ws/
+COPY src /home/rbccps/mrs_ws/src/
+
+
+RUN echo 'source /opt/ros/dashing/setup.bash' >> ~/.bashrc
+
+
---
+++ bringup_dashing_devel.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+#set nocasematch option
+shopt -s nocasematch
+if [[ $(lshw -C display | grep vendor) =~ Nvidia ]]; then
+    nvidiacard=1
+else
+    nvidiacard=0
+fi
+shopt -u nocasematch
+
+#Helper functions
+function retFileNameFromGraphicsCard()
+{
+    if [ $nvidiacard -eq 1 ]; then
+        echo "docker-compose_gpu.yml"
+    elif [ $nvidiacard -eq 0 ]; then
+        echo "docker-compose.yml"
+    fi
+}
+
+function helpFunction()
+{
+    echo ""
+    echo "Usage: $0 -c <build/run>"
+    echo "-c configuration specifying if the container should be built or run"
+    echo "-l flag enables the ELK stack for monitoring logs when run"
+    exit 1
+}
+
+while getopts "c:l" opt
+do
+    case "$opt" in
+        c)
+            cmdargs=$OPTARG
+            ;;
+        l)
+            logger=1
+            ;;
+        \?) #Invalid Option
+            helpFunction
+            ;;
+    esac
+done
+
+if [ $cmdargs == "build" ]; then
+    filename=$(retFileNameFromGraphicsCard)
+    docker-compose -f $filename build
+
+elif [ $cmdargs == "run" ]; then
+    filename=$(retFileNameFromGraphicsCard)
+
+    # Enable ELK stack if log flag is set
+    if [ ! -z $logger ]; then
+        docker-compose -f $filename --profile log up &
+    else
+        docker-compose -f $filename up &
+    fi
+
+    sleep 10
+    #Check if the docker-compose process is running
+    SECONDS=0
+    while true;
+    do
+        if ! pgrep -x "docker-compose" > /dev/null; then
+            duration=$SECONDS
+            if [ $duration -ge 30 ]; then
+                echo "Error"
+                exit 1
+            fi
+        else
+            break
+        fi
+
+    done
+    #Enable container to access the X server of the host
+    xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $(docker ps -l -q)`
+    sleep 10
+
+    echo "The container is running"
+    echo "Press Ctrl-C to stop the container"
+    #NOTE: Will this use up a lot of system resources.
+    #Could stop the bash script but the user must manually kill the docker-compose process
+    #when he/she stops using the container.
+    sleep infinity
+
+else
+    helpFunction
+fi
